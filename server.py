3b2a3df35f8a6a85affeab3b01114b1e272c4dab
tochi <tochi.caraballo@gmail.com>
2016-10-23 15:37:02 -0700

    [challenge 2 complete]

--- server.py
+++ server.py
@@ -2,11 +2,21 @@ import requests
 
 url = 'http://challenge.code2040.org/api/reverse'
 
+submitUrl = 'http://challenge.code2040.org/api/reverse/validate'
+
 temp = {
 	'token':'b622fb971a3ca781b60c8af66f68fe57'
 	}
 
 resp = requests.post(url, data = temp)
 
+stringReverse = resp.text[::-1];
+print (resp.text)
+
+postReverseString = {
+	'token':'b622fb971a3ca781b60c8af66f68fe57',
+	'string': stringReverse
+	}
+resp = requests.post(submitUrl, data = postReverseString)
 
 print (resp.text)
