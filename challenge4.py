da1e1c651a1c1c618634c832b1bc92d8e088efc1
Doanh <tochi.caraballo@gmail.com>
2016-10-25 14:37:49 -0700

    in progress

--- challenge4.py
+++ challenge4.py
@@ -12,50 +12,38 @@ temp = {
 
 resp = requests.post(url, data = temp)
 
-#print (resp.text)
+print (resp.text)
 jsonData = resp.text
 jsonToPython = json.loads(jsonData)
 
 prefix = jsonToPython['prefix']
 arrayWithOutPrefix = jsonToPython['array']
+#print arrayWithOutPrefix
+print ("-----------------")
 
 #finalList = [prefix for prefix in array if not determine(prefix)]
 
-print jsonToPython['array']
+#print jsonToPython['array']
 
 for word in arrayWithOutPrefix[:]:
 	if word.startswith(prefix):
 		arrayWithOutPrefix.remove(word)
 
-#print prefix
-#print arrayWithOutPrefix
+myArray = np.asarray(arrayWithOutPrefix)
 
+encodedArray = json.dumps(myArray)
 
-#myArray = np.asarray(arrayWithOutPrefix)
-#print myArray
-
-#indexOfMatchingWord = array.index(jsonToPython[''])
-
-
-#testing
-"""
-for u in jsonToPython:
-	print('---------------')
-	print jsonToPython[u]
-
-print(jsonToPython['haystack'])
-"""
-encoded_str = json.dumps(arrayWithOutPrefix)
-headers = {'content-type': 'application/json'}
-tok = 'b622fb971a3ca781b60c8af66f68fe57'
+ 
 postReverseString = {
-	'token': tok,
-	'array': arrayWithOutPrefix
+	"token":"b622fb971a3ca781b60c8af66f68fe57",
+	"array": myArray
 	}
+
+#encoded_str = json.dumps(postReverseString)
 
 print('-----------')
 print postReverseString
 
-resp = requests.post(submitUrl, data = json.dumps(postReverseString))
+resp = requests.post(submitUrl, data= json.dumps(postReverseString))
 print (resp.text)
 
